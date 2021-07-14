bac1c0febd2f58b25b028b59f48004b69fea351d
make-github-pseudonymous-again <make-github-pseudonymous-again@users.noreply.github.com>
2021-07-14 15:25:47 +0000

    Deploying to gh-pages from @ iterable-iterator/zip@ccf2d8728c3caa098f09a6819842cf1a2056a500 🚀

--- inject/script/0-header.js
+++ inject/script/0-header.js
@@ -17,7 +17,7 @@ domReady(() => {
 	header.insertBefore(projectname, header.firstChild);
 
 	const testlink = document.querySelector('header > a[data-ice="testLink"]');
-	testlink.href = 'https://coveralls.io/github/iterable-iterator/zip';
+	testlink.href = 'https://app.codecov.io/gh/iterable-iterator/zip';
 	testlink.target = '_BLANK';
 
 	const searchBox = document.querySelector('.search-box');
